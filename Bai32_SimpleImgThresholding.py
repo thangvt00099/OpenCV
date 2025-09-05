fc1c3f5b43d9d4e914112f82180e984740fd6b1a
thangvt00099 <tranvuthang57@gmail.com>
2025-09-05 23:31:25 +0700

    MatplotlibWithThresh

--- Bai32_SimpleImgThresholding.py
+++ Bai32_SimpleImgThresholding.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 img = cv2.imread('D:/OpenCV/img/gradient.png', 0)
-img = cv2.resize(img, (500, 500), interpolation=cv2.INTER_LANCZOS4)
 
 # Ngưỡng nhị phân
 # _, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
@@ -10,12 +10,32 @@ img = cv2.resize(img, (500, 500), interpolation=cv2.INTER_LANCZOS4)
 # Ngưỡng nhị phân nghịch đảo
 # _, th1 = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY_INV)
 
+# Điểm ảnh <= ngưỡng => giữ nguyên
+# Điểm ảnh > ngưỡng => pixel = ngưỡng
 # _, th1 = cv2.threshold(img, 200, 255, cv2.THRESH_TRUNC)
 
-_, th1 = cv2.threshold(img, 200, 255, cv2.THRESH_TOZERO)
+# Điểm ảnh > ngưỡng => giữ nguyên
+# Điểm ảnh <= ngưỡng => pixel = 0
+# _, th1 = cv2.threshold(img, 200, 255, cv2.THRESH_TOZERO)
 
-cv2.imshow('Image', img)
-cv2.imshow('th1', th1)
+_, th1 = cv2.threshold(img, 50, 255, cv2.THRESH_BINARY)
+_, th2 = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY_INV)
+_, th3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
+_, th4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
+_, th5 = cv2.threshold(img,127, 255, cv2.THRESH_TOZERO_INV)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+images = [img, th1, th2, th3, th4, th5]
+
+# cv2.imshow('Image', img)
+# cv2.imshow('th1', th1)
+for i in range(6):
+    plt.subplot(2 ,3, i + 1)
+    plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([])
+    plt.yticks([])
+
+plt.show()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
